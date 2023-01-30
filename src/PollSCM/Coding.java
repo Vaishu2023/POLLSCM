7e8a21c517f195035b2701912e8b05a28d7345ef
prash <prash@192.168.0.42>
2023-01-30 00:23:40 -0500

    Testing Poll SCM

--- src/PollSCM/Coding.java
+++ src/PollSCM/Coding.java
@@ -5,6 +5,7 @@ public class Coding {
 	public static void main(String[] args) {
 		System.out.println("Testing Poll SCM");
 		System.out.println("Learnign Poll SCM");
+		System.out.println("Trying Poll SCM");
 	}
 
 }
